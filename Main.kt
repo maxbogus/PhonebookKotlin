aac4d7d4962e15a600adc91d2312a2eff331b045
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-03-03 02:01:46 +0300

    Update Main.kt
    - added stub for bubble sort and jump search.

--- Main.kt
+++ Main.kt
@@ -5,34 +5,62 @@ import java.sql.Time
 
 const val PATH_TO_FOLDER = "/"
 
-fun main() {
-    val (findFile, leftToFind, directoryFile) = parseInput()
-    println("Start searching...")
-    val startTime = System.currentTimeMillis()
-    performLinearSearch(directoryFile, leftToFind)
-    val endTime = System.currentTimeMillis()
-    val totalTime = endTime - startTime
-    println("Found ${findFile.size} / ${findFile.size} entries. ${getTime(Time(totalTime).time)}")
+data class SearchData(val findFile: List<String>, val leftToFind: List<String>, val directoryFile: List<String>)
 
+fun main() {
+    val searchData = parseInput()
+
+    performLinearSearch(searchData)
+
+    println("Start searching (bubble sort + jump search)...")
+    val sortStartTime = System.currentTimeMillis()
+    val sortedSearchData = performBubbleSort(searchData)
+    val sortEndTime = System.currentTimeMillis()
+    val sortTotalTime = sortStartTime - sortEndTime
+    val jumpSearchStartTime = System.currentTimeMillis()
+    performJumpSearch(sortedSearchData)
+    val jumpSearchEndTime = System.currentTimeMillis()
+    val jumpSearchTotalTime = jumpSearchStartTime - jumpSearchEndTime
+    val totalSortSearchTime = sortTotalTime + jumpSearchTotalTime
+    println(
+        "Found ${sortedSearchData.findFile.size} / ${sortedSearchData.findFile.size} entries. ${
+            getTime(
+                Time(
+                    totalSortSearchTime
+                ).time
+            )
+        }"
+    )
+    println("Sorting time: ${getTime(Time(sortTotalTime).time)}")
+    println("Searching time: ${getTime(Time(jumpSearchTotalTime).time)}")
 }
 
-private fun parseInput(): Triple<List<String>, List<String>, List<String>> {
+fun performBubbleSort(searchData: SearchData): SearchData {
+    return searchData
+}
+
+fun performJumpSearch(sortedSearchData: SearchData) {
+    TODO()
+}
+
+private fun parseInput(): SearchData {
     val findFile = File("${PATH_TO_FOLDER}find.txt").readLines()
     val leftToFind = mutableListOf<String>()
     for (item in findFile) {
         leftToFind.add(item)
     }
     val directoryFile = File("${PATH_TO_FOLDER}directory.txt").readLines()
-    return Triple(findFile, leftToFind, directoryFile)
+    return SearchData(findFile, leftToFind, directoryFile)
 }
 
 private fun performLinearSearch(
-    directoryFile: List<String>,
-    leftToFind: List<String>
+    searchData: SearchData
 ) {
+    println("Start searching (linear search)...")
+    val startTime = System.currentTimeMillis()
     val result = mutableListOf<String>()
-    for (line in directoryFile) {
-        for (needle in leftToFind) {
+    for (line in searchData.directoryFile) {
+        for (needle in searchData.leftToFind) {
             if (line.contains(needle)) {
                 if (!result.contains(needle)) {
                     result.add(line)
@@ -40,6 +68,9 @@ private fun performLinearSearch(
             }
         }
     }
+    val endTime = System.currentTimeMillis()
+    val totalTime = endTime - startTime
+    println("Found ${searchData.findFile.size} / ${searchData.findFile.size} entries. ${getTime(Time(totalTime).time)}")
 }
 
 fun getTime(time: Long): String {
